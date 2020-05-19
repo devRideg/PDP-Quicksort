1d97f910b22ec99ffa9fa59329aa1f1a019a6956
Skrot-och-co <61704717+Skrot-och-co@users.noreply.github.com>
2020-05-19 22:23:08 +0200

    Happy days

---
+++ QuicksortMPI_Final2/Quicksort.h
@@ -0,0 +1,8 @@
+/**
+ * Main function for parallel quicksort
+ * 
+ * Written for assignment 3 of the PDP course.
+ * Authors - Dennis Jensen and Johan Rideg 2020.
+ */
+#include "qsMPI.h"
+
---
+++ QuicksortMPI_Final2/checkSorted.c
@@ -0,0 +1,31 @@
+/**
+ * Small program to check if data is sorted in descending order
+ */
+#include "checkSorted.h"
+
+int main(int argc, char *argv[])
+{
+    int n, i, *arr, count = 0;
+    n = atoi(argv[2]);
+
+    read_input2(&arr, argv[1], n);
+
+    if(n < 0) return n;
+
+    for (i = 1; i < n; i++)
+    {
+        if(arr[i] < arr[i-1]) count++;
+    }
+
+    if(count == 0)
+    {
+        printf("Array is correctly sored in descending order!\n");
+    }
+    else
+    {
+        printf("Array does not follow decending order for %d elements.\n", count);
+    }
+
+    free(arr);
+    return 0;
+}
